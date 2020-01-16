f4785a5276738ab835fa6d8ee5b9c79f458ffabb
Skyler Dowdy <skylerwebdev@gmail.com>
2020-01-16 14:29:40 -0500

    BASIC Reactive Nav Bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,29 +2,39 @@ import React, {useState} from 'react'
 import { NavLink as NL } from 'react-router-dom'
 import '../styles/Nav.css'
 
-const showNav = () => {
-    const navLinks = document.querySelectorAll('.nL')
-    console.log('clicked', navLinks)
-    // navLinks.classList.remove('hidden')
-}
+
+
 
 
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false)
+
+    const shownav = () => {
+    if(open === true){
+        setOpen(false)
+        console.log("clicked")
+    } if(open === false)
+    {
+        setOpen(true)
+    }
+    }
+
+
     return (
         <div className="mN">
             <div className="nLt">
-            <span className="nL hide" onClick={showNav}  >Open Nav Container</span>
+            <span className="open hide"  onClick={shownav}>Open Nav Container</span>
             </div>
-            <div className="nCt">
-            <NL className="nL hidden" exact to='/'>Home</NL>
-            <NL className="nL hidden" to='#'>About</NL>
-            <NL className="nL hidden" to='#'>Developers</NL>
-            <NL className="nL hidden" to='#'>Consumers</NL>
+            <div className={open ? "nCt" : "nCt hidden"}>
+            <NL className="nL" exact to='/'>Home</NL>
+            <NL className="nL" to='#'>About</NL>
+            <NL className="nL" to='#'>Developers</NL>
+            <NL className="nL" to='#'>Consumers</NL>
             </div>
-            <div className="nRt">
-            <NL className="nL hidden" to='#'>Login</NL>
-            <NL className="nL hidden" to='#'>Register</NL>
+            <div className={open ? "nRt" : "nRt hidden"}>
+            <NL className="nL" to='#'>Login</NL>
+            <NL className="nL" to='#'>Register</NL>
             </div>
         </div>
     )
