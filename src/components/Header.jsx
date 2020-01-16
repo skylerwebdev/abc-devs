1269bfdd102957a13cfdd45966672750852380e5
Skyler Dowdy <skylerwebdev@gmail.com>
2020-01-16 13:12:34 -0500

    initial-commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './NavBar'
+const Header = () => {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default Header
