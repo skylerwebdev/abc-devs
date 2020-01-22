6739d7acf9a0430fe70a0a67cbb4062558b9e3b9
Skyler Dowdy <skylerwebdev@gmail.com>
2020-01-22 16:49:03 -0500

    Login Form

--- src/configs/AppRouter.jsx
+++ src/configs/AppRouter.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Route as R } from 'react-router-dom'
+import { Route as R, Switch} from 'react-router-dom'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Hello from '../pages/Hello'
 const AppRouter = () => {
     return (
         <div>
+            <Switch>
             <R exact path='/'>{Home}</R>
+            <R path='/hello'>{Hello}</R>
+    <R exact path='/login'>{Login}</R>
+    </Switch>
         </div>
     )
 }
