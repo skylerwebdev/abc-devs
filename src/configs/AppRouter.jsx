308c8fd0329aba09f586effa7d309bfc6c388b6e
Skyler Dowdy <skylerwebdev@gmail.com>
2020-01-25 18:26:04 -0500

    Original Site

--- src/configs/AppRouter.jsx
+++ src/configs/AppRouter.jsx
@@ -3,11 +3,13 @@ import { Route as R, Switch} from 'react-router-dom'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Hello from '../pages/Hello'
+import About from '../pages/About'
 const AppRouter = () => {
     return (
         <div>
             <Switch>
             <R exact path='/'>{Home}</R>
+    <R path="/about">{About}</R>
             <R path='/hello'>{Hello}</R>
     <R exact path='/login'>{Login}</R>
     </Switch>
